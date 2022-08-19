8bd345ee6411708a12debafb30b1d049a83e2bfc
Kelvin Kahihu <kahihu35@gmail.com>
2022-08-19 10:20:34 +0300

    Changes

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <!-- Jumbotron -->
+    <div class="text-center bg-gray-50 text-gray-800 py-20 px-6">
+      <
+      <a class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Get started</a>
+    </div>
+    <!-- Jumbotron -->
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: "Header-View"
+}
+</script>
+
+<style scoped>
+
+</style>
