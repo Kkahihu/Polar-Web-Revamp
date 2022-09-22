902b5f2e707dc769afb2a91b7c46e210d96a4e52
Kelvin Kahihu <kahihu35@gmail.com>
2022-09-22 08:15:40 +0300

    Create Products.vue

---
+++ components/Products.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <div class="py-16 bg-gray-50 overflow-hidden">
+      <div class="container m-auto px-6 space-y-8 text-gray-500 md:px-12">
+        <div class="my-3 mb-5 flex justify-center">
+          <span class="bg-orange-100 text-orange-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-800">Products</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ProductsView"
+}
+</script>
+
+<style scoped>
+
+</style>
