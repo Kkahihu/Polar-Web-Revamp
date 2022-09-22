de8f0061f01842ef53cdde2993b40f018e784ec9
Kelvin Kahihu <kahihu35@gmail.com>
2022-09-22 08:15:28 +0300

    Create Features.vue

---
+++ components/Features.vue
@@ -0,0 +1,152 @@
+<template>
+  <section>
+<!--    <div class="grid grid-cols-2 gap-2">-->
+<!--      <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">-->
+<!--        <a href="#">-->
+<!--          <img class="rounded-t-lg" src="" alt="">-->
+<!--        </a>-->
+<!--        <div class="p-5">-->
+<!--          <a href="#">-->
+<!--            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>-->
+<!--          </a>-->
+<!--          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>-->
+<!--          <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">-->
+<!--            Read more-->
+<!--            <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>-->
+<!--          </a>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">-->
+<!--        <a href="#">-->
+<!--          <img class="rounded-t-lg" src="" alt="">-->
+<!--        </a>-->
+<!--        <div class="p-5">-->
+<!--          <a href="#">-->
+<!--            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>-->
+<!--          </a>-->
+<!--          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>-->
+<!--          <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">-->
+<!--            Read more-->
+<!--            <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>-->
+<!--          </a>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">-->
+<!--        <a href="#">-->
+<!--          <img class="rounded-t-lg" src="" alt="">-->
+<!--        </a>-->
+<!--        <div class="p-5">-->
+<!--          <a href="#">-->
+<!--            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>-->
+<!--          </a>-->
+<!--          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>-->
+<!--          <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">-->
+<!--            Read more-->
+<!--            <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>-->
+<!--          </a>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">-->
+<!--        <a href="#">-->
+<!--          <img class="rounded-t-lg" src="" alt="">-->
+<!--        </a>-->
+<!--        <div class="p-5">-->
+<!--          <a href="#">-->
+<!--            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>-->
+<!--          </a>-->
+<!--          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>-->
+<!--          <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">-->
+<!--            Read more-->
+<!--            <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>-->
+<!--          </a>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+
+
+    <div class="py-16 bg-gray-50 overflow-hidden">
+      <div class="container m-auto px-6 space-y-8 text-gray-500 md:px-12">
+        <div class="my-3 mb-5 flex justify-center">
+          <span class="bg-orange-100 text-orange-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-800">Features</span>
+        </div>
+        <div>
+<!--          <span class="text-gray-600 text-lg font-semibold">Main features</span>-->
+          <h2 class="mt-4 text-2xl text-gray-900 font-bold md:text-4xl">A technology-first approach to geolocation <br class="lg:block" hidden> and analytics</h2>
+        </div>
+        <div class="mt-16 grid border divide-x divide-y rounded-xl overflow-hidden sm:grid-cols-2 lg:divide-y-0 lg:grid-cols-3 xl:grid-cols-4">
+          <div class="relative group bg-white transition hover:z-[1] hover:shadow-2xl">
+            <div class="relative p-8 space-y-8">
+              <img src="images/avatars/burger.png" class="w-10" width="512" height="512" alt="burger illustration">
+
+              <div class="space-y-2">
+                <h5 class="text-xl text-gray-800 font-medium transition group-hover:text-yellow-600">Drone and satellite imagery-based mapping</h5>
+                <p class="text-sm text-gray-600">We provide a wide array of mapping products utilizing autonomous drones with sensors, earth observation satellite imagery, and global positioning systems (GPS) sensors depending on the client's use case and scale of work.</p>
+              </div>
+              <a href="#" class="flex justify-between items-center group-hover:text-yellow-600">
+                <span class="text-sm">Read more</span>
+                <span class="-translate-x-4 opacity-0 text-2xl transition duration-300 group-hover:opacity-100 group-hover:translate-x-0">&RightArrow;</span>
+              </a>
+            </div>
+          </div>
+          <div class="relative group bg-white transition hover:z-[1] hover:shadow-2xl">
+            <div class="relative p-8 space-y-8">
+              <img src="images/avatars/trowel.png" class="w-10" width="512" height="512" alt="burger illustration">
+
+              <div class="space-y-2">
+                <h5 class="text-xl text-gray-800 font-medium transition group-hover:text-yellow-600">
+                  Geospatial data visualization and analytics</h5>
+                <p class="text-sm text-gray-600">With our spatial analysis and mapping expertise, we provide powerful tools to model and analyze spatial data, enabling you to identify relationships and trends that would not otherwise be apparent.</p>
+              </div>
+              <a href="#" class="flex justify-between items-center group-hover:text-yellow-600">
+                <span class="text-sm">Read more</span>
+                <span class="-translate-x-4 opacity-0 text-2xl transition duration-300 group-hover:opacity-100 group-hover:translate-x-0">&RightArrow;</span>
+              </a>
+            </div>
+          </div>
+          <div class="relative group bg-white transition hover:z-[1] hover:shadow-2xl">
+            <div class="relative p-8 space-y-8">
+              <img src="images/avatars/package-delivery.png" class="w-10" width="512" height="512" alt="burger illustration">
+
+              <div class="space-y-2">
+                <h5 class="text-xl text-gray-800 font-medium transition group-hover:text-yellow-600">
+                  Geospatial Artificial Intelligence (Geo-AI)</h5>
+                <p class="text-sm text-gray-600">We combine geospatial big data with artificial intelligence applications for high-performance computing for data mining, spatial big data analysis, and automatic feature extraction from satellite/drone images.</p>
+              </div>
+              <a href="#" class="flex justify-between items-center group-hover:text-yellow-600">
+                <span class="text-sm">Read more</span>
+                <span class="-translate-x-4 opacity-0 text-2xl transition duration-300 group-hover:opacity-100 group-hover:translate-x-0">&RightArrow;</span>
+              </a>
+            </div>
+          </div>
+          <div class="relative group bg-gray-100 transition hover:z-[1] hover:shadow-2xl lg:hidden xl:block">
+            <div class="relative p-8 space-y-8 border-dashed rounded-lg transition duration-300 group-hover:bg-white group-hover:border group-hover:scale-90">
+              <img src="images/avatars/metal.png" class="w-10" width="512" height="512" alt="burger illustration">
+
+              <div class="space-y-2">
+                <h5 class="text-xl text-gray-800 font-medium transition group-hover:text-yellow-600">More features</h5>
+                <p class="text-sm text-gray-600">Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.</p>
+              </div>
+              <a href="#" class="flex justify-between items-center group-hover:text-yellow-600">
+                <span class="text-sm">Read more</span>
+                <span class="-translate-x-4 opacity-0 text-2xl transition duration-300 group-hover:opacity-100 group-hover:translate-x-0">&RightArrow;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </section>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Features"
+}
+</script>
+
+<style scoped>
+
+</style>
